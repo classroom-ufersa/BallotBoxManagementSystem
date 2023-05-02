6aa61ba62d2594bbee5fdead2fcfb7fe3fe229be
JCarlosSousa <carlossousa.gm@gmail.com>
2023-05-02 11:49:15 -0300

    Adicionado as Structs, Funções, e alguns detalhes na main.

---
+++ BallotBox_C/main.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "urnas.c"
+#define MAX_CANDIDATOS 5
+
+int main (void) {
+    Candidato *candidato[MAX_CANDIDATOS]; 
+    char nome[50], partido[50], vice[50], estado[2];
+    int idade, numero, n_candidatos;
+    
+    printf("Bem-vindo ao Sistema de Gerenciamento de Urnas!\n");
+    int i, escolha = 0;
+    while (escolha != 7) {
+        printf("1. Adicionar candidato\n");
+        printf("2. Remover candidato\n");
+        printf("3. Listar candidatos cadastrados\n");
+        printf("4. Buscar candidato\n");
+        printf("5. Editar candidato\n");
+        printf("6. Consultar localização de urna\n");
+        printf("7. Sair\n");
+        scanf("%d", &escolha);
+        
+        switch (escolha) {
+            case 1:
+                if(n_candidatos < MAX_CANDIDATOS-1){
+                    printf("Digite o seu nome: ");
+                    scanf("%[^\n]s", nome);
+                    printf("Informe a sua idade: ");
+                    scanf("%d", &idade);
+                    prinft("Digite o seu numero: ");
+                    scanf("%d", &numero);
+                    printf("Informe o seu partido: ");
+                    scanf("%[^\n]s", partido);
+                    printf("Digite o nome do seu vice: ");
+                    scanf("%[^\n]s", vice);
+                    printf("Informe o seu estado: ");
+                    scanf("%[^\n]s", estado);
+
+                    Candidato *adiciona_candidato = adicionar_candidato(nome, idade, numero, partido, vice, estado);
+                    candidato[n_candidatos++] = adiciona_candidato;
+
+                } else {
+                    printf("NUMERO MAXIMO DE CANDIDATOS ATINGIDO!\n");
+                }
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                printf("Obrigado por usar este programa.\n");
+                exit(1);
+                break;
+            default:
+                printf("Opção inválida! Tente novamente.\n");
+                break;
+        }
+    }
+}
--- BallotBox_C/urnas.c
+++ BallotBox_C/urnas.c
@@ -3,12 +3,14 @@
 #include <stdio.h>
 #include <ctype.h>
 #include "urnas.h"
+#define MAX_CANDIDATOS 5
 
 typedef struct urna{
-    char codigo[20];
+    char codigo_verificacao[10];
+    char localizacao[100];
     int num_candidatos;
-    Candidato candidatos[50];
-    int votos[50];
+    Candidato candidatos[MAX_CANDIDATOS];
+    int votos[MAX_CANDIDATOS];
 };
 
 typedef struct candidato{
@@ -19,3 +21,130 @@ typedef struct candidato{
     char vice[50];
     char estado[2];
 };
+
+/*Função para adicionar candidatos*/
+Candidato *adicionar_candidato(char *nome, int idade, int numero, char *partido, char *vice, char *estado){
+    Candidato *candidato = (Candidato*)malloc(sizeof(Candidato));
+    if (candidato == NULL){
+        printf("Memoria insuficiente!\n");
+        exit(1);
+    }
+    nome[0] = toupper(nome[0]);
+    strcpy(candidato->nome, nome);
+    candidato->idade = idade;
+    candidato->numero = numero;
+    strcpy(candidato->partido, partido);
+    strcpy(candidato->vice, vice);
+    strcpy(candidato->estado, estado);
+}
+
+/*Função para remover candidato*/
+void remover_candidato(Urna *urna){
+    int numero;
+    
+    printf("Numero do candidato a ser removido: ");
+    scanf("%d", &numero);
+    
+    for (int i = 0; i < urna->num_candidatos; i++) {
+        if (urna->candidatos[i].numero == numero) {
+            for (int j = i; j < urna->num_candidatos - 1; j++) {
+                urna->candidatos[j] = urna->candidatos[j + 1];
+                urna->votos[j] = urna->votos[j + 1];
+            }
+            
+            urna->num_candidatos--;
+            
+            printf("Candidato removido com sucesso.\n");
+            return;
+        }
+    }
+    
+    printf("Candidato não encontrado.\n");
+}
+
+/*Função para listar os candidatos cadastrados*/
+void listar_candidatos(Urna *urna){
+    for (int i = 0; i < urna->num_candidatos; i++) {
+        Candidato c = urna->candidatos[i];
+        
+        printf("%d - %s (%s)\n", c.numero, c.nome, c.partido);
+        printf("Idade: %d\n", c.idade);
+        printf("Vice: %s\n", c.vice);
+        printf("Estado: %s\n", c.estado);
+        printf("\n");
+    }
+}
+
+/*Função para buscar candidato*/
+void buscar_candidato(Urna *urna){
+    int numero;
+    
+    printf("Numero do candidato a ser buscado: ");
+    scanf("%d", &numero);
+    
+    for (int i = 0; i < urna->num_candidatos; i++) {
+        if (urna->candidatos[i].numero == numero) {
+            Candidato c = urna->candidatos[i];
+            
+            printf("%d - %s (%s)\n", c.numero, c.nome, c.partido);
+            printf("Idade: %d\n", c.idade);
+            printf("Vice: %s\n", c.vice);
+            printf("Estado: %s\n", c.estado);
+            
+            return;
+        }
+    }
+    
+    printf("Candidato não encontrado.\n");
+}
+
+/*Função para editar candidato*/
+void editar_candidato(Urna *urna){
+    int numero;
+    
+    printf("Número do candidato a ser editado: ");
+    scanf("%d", &numero);
+    
+    for (int i = 0; i < urna->num_candidatos; i++) {
+        if (urna->candidatos[i].numero == numero) {
+            Candidato *candidato = &urna->candidatos[i];
+            
+            printf("Novo nome (%s): ", candidato->nome);
+            scanf("%s", candidato->nome);
+            
+            printf("Nova idade (%d): ", candidato->idade);
+            scanf("%d", &candidato->idade);
+            
+            printf("Novo partido (%s): ", candidato->partido);
+            scanf("%s", candidato->partido);
+            
+            printf("Novo vice (%s): ", candidato->vice);
+            scanf("%s", candidato->vice);
+            
+            printf("Novo estado (%s): ", candidato->estado);
+            scanf("%s", candidato->estado);
+            
+            printf("Candidato editado.\n");
+            return;
+        }
+    }
+    
+    printf("Candidato não encontrado.\n");
+}
+
+/*Função para localizar uma urna*/
+void consultar_localizacao_urna(Urna *urnas, int num_urnas){
+    char codigo[10];
+    
+    printf("Código de verificação da urna: ");
+    scanf("%s", codigo);
+    
+    for (int i = 0; i < num_urnas; i++) {
+        if (strcmp(urnas[i].codigo_verificacao, codigo) == 0) {
+            printf("Localização: %s\n", urnas[i].localizacao);
+            return;
+        }
+    }
+    
+    printf("Urna não encontrada.\n");
+}
